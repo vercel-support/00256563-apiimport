593f6c59e0ab84bd2535bcfd825dee12d8c2bc93
Nick Vigier <nvigier46@gmail.com>
2024-07-18 15:42:48 +0200

    add vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [react(), vercel()],
+});
